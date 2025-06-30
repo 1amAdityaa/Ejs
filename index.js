4d4beab11050b541249ee2a4cc95e94881314a78
1amAdityaa <adityaraj76692@gmail.com>
2025-06-30 19:51:35 +0530

    ejs

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ app.get("/", (req,res) => {
     res.render("home.ejs");
 });
 
-app.get("/ig/:username", (req,res) => {
+/* app.get("/ig/:username", (req,res) => {
     const followers = ["adam","bob","steve", "abc"];
     let { username } = req.params ; //username is embedded using .params
      res.render("Instagram.ejs" , {username, followers});
 });
-
+*/
+app.get("/ig/:username", (req,res) => {
+    let {username} = req.params;
+    const instaData = require("./data.json");
+    console.log(instaData);
+    const data = instaData[username];
+    console.log(data);
+    res.render("instagram.ejs", {data});
+});
 
 app.get("/rolldice" , (req,res) => {
     let diceValue = Math.floor(Math.random() * 6) + 1;
